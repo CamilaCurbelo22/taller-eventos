d8bcab572fea0f0b1b9d40c61160ed627d0bfd41
Camila Curbelo <88462748+CamilaCurbelo22@users.noreply.github.com>
2023-08-09 19:12:59 -0300

    Alerta y estilo en el div creados

---
+++ eventos.js
@@ -0,0 +1,12 @@
+const theDiv = document.getElementById("theDiv");
+const theButton = document.getElementById("theButton");
+
+
+theDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
+
+theButton.addEventListener("click", function() {
+    alert("Hola!");
+});
+
